d97862161f3781c15696dfc449a717ad22cd4161
Jeff Mead <jeffdmead171@gmail.com>
2020-08-24 14:01:28 -0400

    added security and authentication

--- models/User.js
+++ models/User.js
@@ -3,7 +3,12 @@ const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
 // create our User model
-class User extends Model {}
+class User extends Model {
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 // define table columns and configuration
   User.init(
@@ -47,6 +52,7 @@ class User extends Model {}
           return updatedUserData;
         }
       },
+
       sequelize,
       timestamps: false,
       freezeTableName: true,
