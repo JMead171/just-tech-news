52fe35b04bb847840840df9b0a7ca8b8fc850b11
Jeff Mead <jeffdmead171@gmail.com>
2020-08-31 16:35:24 -0400

    added single post page

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -10,19 +10,23 @@ router.get('/', (req, res) => {
     });
 });
 
-router.post('/', (req, res) => {
-    Comment.create({
+router.post('/', (req, res) => {  
+    // check the session
+    if (req.session) {
+      Comment.create({
         comment_text: req.body.comment_text,
-        user_id: req.body.user_id,
-        post_id: req.body.post_id
-        })
+        post_id: req.body.post_id,
+        // use the id from the session
+        user_id: req.session.user_id
+      })
         .then(dbCommentData => res.json(dbCommentData))
         .catch(err => {
-            console.log(err);
-            res.status(400).json(err);
+          console.log(err);
+          res.status(400).json(err);
         });
-});
-
+    }
+  });
+  
 router.delete('/:id', (req, res) => {
     Comment.destroy({
     where: {
@@ -41,5 +45,7 @@ router.delete('/:id', (req, res) => {
         res.status(500).json(err);
       });
 });
+
+
 
 module.exports = router;
